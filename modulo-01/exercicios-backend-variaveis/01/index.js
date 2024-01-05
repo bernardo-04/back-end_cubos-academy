d914211166b6d125ad8f593cc2c035165a0a74e2
Bernardo <bernardo.bcruz@gmail.com>
2024-01-05 14:09:33 -0300

    fix: arquivos que não são do modulo 02 removidos

--- modulo-01/exercicios-backend-variaveis/01/index.js
+++
@@ -1,9 +0,0 @@
-// Autor: Bernardo de Brito Cruz
-// Curso de Desenvolvimento de Software com foco em Back-end da Cubos Academy
-// Calculadora de IMC
-
-let peso = 76;
-let altura = 1.79;
-const imc = peso / altura ** 2;
-
-console.log(`O seu IMC é: ${imc.toFixed(1)}`);
